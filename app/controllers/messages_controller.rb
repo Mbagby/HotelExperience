3713b16d502097a7314f6f56842236b1e5a9a737
Madeline <madelinebagby@yahoo.com>
2015-09-30 13:51:57 -0700

    Finished the base for the front desk chat app

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,15 @@
 class MessagesController < ApplicationController
   def index
   	@user = User.find_by_id(session[:user_id])
-    @messages = Message.all
+    @messages = Message.last(10)
+    @users = User.all
   end
 
 
   def create
   	@user = User.find_by_id(session[:user_id])
-  	@message = Message.create(:content=> :content, :user_id=> @user.id)
-    PrivatePub.publish_to("/messages")
+  	@message = @user.messages.create message_params
+    @message.user_id = @user.id
   end
 
 
