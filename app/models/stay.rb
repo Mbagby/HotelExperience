7802ac83554fa6b63412fd8cceb343b9fce1ce6c
Madeline <madelinebagby@yahoo.com>
2015-10-04 21:33:56 -0700

    reformatting and fixing responsive design again

--- app/models/stay.rb
+++ app/models/stay.rb
@@ -1,7 +1,7 @@
 class Stay < ActiveRecord::Base
 	belongs_to :user
 	has_many :work_orders, dependent: :destroy
-	has_many :day_services, dependent: :destroy
+	has_many :dayservices, dependent: :destroy
 	has_many :eveningservices, dependent: :destroy
 	has_one :hotel
 	has_many :rooms
